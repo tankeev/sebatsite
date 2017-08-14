c4dcd0744dd7bab26b7fea2d4658469411d49d13
tankeev <tankeev@gmail.com>
2017-08-15 00:42:57 +0600

    experiment with css move aaround

--- sebatsite/settings/local.py
+++ sebatsite/settings/local.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'news\static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'news\static','sebatsite\settings\static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
