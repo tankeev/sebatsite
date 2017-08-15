65ea66805639c1c146ca1abbb2705801c0b44f96
tankeev <tankeev@gmail.com>
2017-08-16 00:40:45 +0600

    set cha

--- sebatsite/settings.py
+++ sebatsite/settings.py
@@ -135,20 +135,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-ALLOWED_HOSTS = ['*']
-
-DEBUG = False
-
-try:
-    from .local_settings import *
-except ImportError:
-    pass
-
-
-
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
