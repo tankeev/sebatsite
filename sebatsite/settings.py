4adf01524015063ff3ac8957dee3ed66a913d94e
tankeev <tankeev@gmail.com>
2017-08-16 02:21:08 +0600

    change to settings.pydfghdfhd

--- sebatsite/settings.py
+++ sebatsite/settings.py
@@ -13,9 +13,9 @@ import os
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-
+print(BASE_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'sebatsite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'site\db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
